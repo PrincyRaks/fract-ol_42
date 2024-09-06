83c9e52723405fee704c50e7f34663012ac0efe9
Rovasoanirina Princy Rakotosalama <rrakotos@c1r9s13.42antananarivo.mg>
2024-09-06 17:09:36 +0300

    test minilibx

--- mandatory/fractol.h
+++ mandatory/fractol.h
@@ -6,7 +6,7 @@
 /*   By: rrakotos <rrakotos@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 10:44:58 by rrakotos          #+#    #+#             */
-/*   Updated: 2024/09/05 14:56:28 by rrakotos         ###   ########.fr       */
+/*   Updated: 2024/09/06 15:37:30 by rrakotos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,23 @@
 # include "../minilibx/mlx.h"
 # include <stdio.h>
 # include <stdlib.h>
-# include <unistd.h>
+#include <unistd.h>
+
+#  define WINDOW_WIDTH 1920
+#  define WINDOW_HEIGHT 1080
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+typedef struct s_point
+{
+	int		x;
+	int		y;
+}			t_point;
 
 #endif
--- mandatory/main.c
+++ mandatory/main.c
@@ -1,12 +1,53 @@
 #include "fractol.h"
 
-int main(void)
+void	put_pixel(t_data *data, t_point point, int color)
 {
-    void    *mlx;
-    void    *frame;
+	char	*dst;
 
-    mlx = mlx_init();
-    frame = mlx_new_window(mlx, 500, 500, "Frame");
-    mlx_loop(mlx);
-    return (0);
+	if ((point.x >= 0 && point.x <= WINDOW_WIDTH) && (point.y >= 0
+			&& point.y <= WINDOW_HEIGHT))
+	{
+		dst = data->addr + (point.y + data->line_length + point.x
+				* (data->bits_per_pixel / 8));
+		*(unsigned int *)dst = color;
+	}
+}
+
+void	draw_rectangle(t_data *image, t_point point, int width, int height)
+{
+	int	x_end;
+	int	y_end;
+	int	color;
+
+	x_end = point.x + width;
+	y_end = point.y + height;
+	color = 0x00FFABCC;
+	while (point.x <= x_end && point.y <= y_end)
+	{
+		put_pixel(image, point, color);
+		point.x++;
+		point.y++;
+	}
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*frame;
+	t_data	image;
+	t_point	point;
+
+	mlx = mlx_init();
+	if (!mlx)
+		perror("Error for initializing mlx");
+	frame = mlx_new_window(mlx, 400, 400, "Frame");
+	image.img = mlx_new_image(mlx, 400, 500);
+	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel,
+			&image.line_length, &image.endian);
+	point.x = 1;
+	point.y = 1;
+	draw_rectangle(&image, point, 50, 50);
+	mlx_put_image_to_window(mlx, frame, image.img, point.x, point.y);
+	mlx_loop(mlx);
+	return (0);
 }
